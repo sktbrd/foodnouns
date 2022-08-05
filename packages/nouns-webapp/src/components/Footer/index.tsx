d9fdf498ec0731dc35db9fe45040662cbd7041db
yanuar-ar <yanuar.ar@gmail.com>
2022-08-05 07:50:03 +0700

    remove forum footer menu

--- packages/nouns-webapp/src/components/Footer/index.tsx
+++ packages/nouns-webapp/src/components/Footer/index.tsx
@@ -10,7 +10,6 @@ const Footer = () => {
   const twitterURL = externalURL(ExternalURL.twitter);
   const discordURL = externalURL(ExternalURL.discord);
   const etherscanURL = buildEtherscanAddressLink(config.addresses.nounsToken);
-  const discourseURL = externalURL(ExternalURL.discourse);
 
   return (
     <div className={classes.wrapper}>
@@ -19,7 +18,6 @@ const Footer = () => {
           <Link text={<Trans>Discord</Trans>} url={discordURL} leavesPage={true} />
           <Link text={<Trans>Twitter</Trans>} url={twitterURL} leavesPage={true} />
           <Link text={<Trans>Etherscan</Trans>} url={etherscanURL} leavesPage={true} />
-          <Link text={<Trans>Forums</Trans>} url={discourseURL} leavesPage={false} />
         </footer>
       </Container>
     </div>
