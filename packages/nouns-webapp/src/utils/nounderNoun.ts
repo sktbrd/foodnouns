7c320a2c418d2108767aed58db580d8993277628
yanuar-ar <yanuar.ar@gmail.com>
2022-08-05 07:52:30 +0700

    add eq(1)

--- packages/nouns-webapp/src/utils/nounderNoun.ts
+++ packages/nouns-webapp/src/utils/nounderNoun.ts
@@ -3,7 +3,7 @@ import { AuctionState } from '../state/slices/auction';
 import { BigNumber } from '@ethersproject/bignumber';
 
 export const isNounderNoun = (nounId: BigNumber) => {
-  return nounId.mod(10).eq(0) || nounId.eq(0) || nounId.mod(10).eq(1);
+  return nounId.mod(10).eq(0) || nounId.eq(0) || nounId.mod(10).eq(1) || nounId.eq(1);
 };
 
 const emptyNounderAuction = (onDisplayAuctionId: number): Auction => {
